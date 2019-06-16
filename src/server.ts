a841a14a42a982b4cb7a8419446eb3a6824518e5
Aliaksei <a.d.makas@gmail.com>
2019-06-16 23:56:37 +0300

    fix: show port from heroku

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(PORT, () => console.log("Example app listening on port 3000!"));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
